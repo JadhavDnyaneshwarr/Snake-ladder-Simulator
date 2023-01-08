68e86d389ba268f2806b7264b813413a24674d5e
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-08 16:11:08 +0530

    player rolls the dice to get value between 1 to 6.

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadderSimulator.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.snakeandladdergame;
 
 public class SnakeAndLadderSimulator {
+    static int playerPosition = 0;
     public static void main(String[] args) {
         System.out.println("welcome to Snake and Ladder simulator");
-        int playerPosition = 0;
+        playerPosition = ((int) (Math.random()*10)%6)+1;
+        System.out.println(playerPosition);
     }
 }
