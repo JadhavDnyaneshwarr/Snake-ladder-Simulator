ae34ee3ae0a19e038fdb925b1dc84e0d4d8b8a7b
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-08 15:39:54 +0530

    Welcome to Snake and Ladder Simulator.

---
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadderSimulator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.snakeandladdergame;
+
+public class SnakeAndLadderSimulator {
+    public static void main(String[] args) {
+        System.out.println("welcome to Snake and Ladder simulator");
+    }
+}
