7dc7590bbb489298f8a0b38cf7724251b18cf2f9
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-08 15:47:21 +0530

    player position at start.

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadderSimulator.java
@@ -3,5 +3,6 @@ package com.bridgelabz.snakeandladdergame;
 public class SnakeAndLadderSimulator {
     public static void main(String[] args) {
         System.out.println("welcome to Snake and Ladder simulator");
+        int playerPosition = 0;
     }
 }
