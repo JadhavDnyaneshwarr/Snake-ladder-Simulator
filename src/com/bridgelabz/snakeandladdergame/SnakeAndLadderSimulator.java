daae184ab9647ce9c2723274161ddcabc15a13d7
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-09 15:28:30 +0530

    player get option for play.

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadderSimulator.java
@@ -1,10 +1,28 @@
 package com.bridgelabz.snakeandladdergame;
 
 public class SnakeAndLadderSimulator {
-    static int playerPosition = 0;
+    static int dice = 0;
+    static int playerOption = 0;
     public static void main(String[] args) {
         System.out.println("welcome to Snake and Ladder simulator");
-        playerPosition = ((int) (Math.random()*10)%6)+1;
-        System.out.println(playerPosition);
+        dice = ((int) (Math.random()*10)%6)+1;
+        System.out.println("dice value " + dice);
+        playerOption = ((int) (Math.random()*10)%3);
+        System.out.println("player get option " + playerOption);
+        int playerPosition = 0;
+        switch (playerOption){
+            case 0 :
+                System.out.println("player stay at same position");
+                break;
+            case 1 :
+                playerPosition = playerPosition + dice;
+                break;
+            case 2 :
+                playerPosition = playerPosition - dice;
+                break;
+
+
+        }
+        System.out.println("player is on " + playerPosition);
     }
 }
